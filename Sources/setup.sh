4b273635428b9d7d58a26a7ee343b9a566cd743a
Rodrigo Garcês <rmgarces1@gmail.com>
2019-03-03 19:04:22 -0300

    Updates scripts

--- Sources/setup.sh
+++ Sources/setup.sh
@@ -2,9 +2,11 @@
 
 home=$PWD #Store current directory
 
-echo "this tutorial is based on https://github.com/herodrigues/robocup2d-tutorial/blob/master/sections/installing-the-soccer-simulator.md"
-echo "fedit2 installation is based on https://www.linuxquestions.org/questions/ubuntu-63/error-in-installing-fedit2-4175632099/"
+echo "This tutorial is based on https://github.com/herodrigues/robocup2d-tutorial/blob/master/sections/installing-the-soccer-simulator.md"
+echo -e "Fedit2 installation is based on https://www.linuxquestions.org/questions/ubuntu-63/error-in-installing-fedit2-4175632099/\n\n"
 echo "Install process can take 5~30 min, deppending on your pc and internet speed"
+echo "Script developed by Rodrigo Garcês"
+echo "UFMA 2018-2019"
 
 echo "Installing dependencies"
 sudo apt-get install -y g++ build-essential libboost-all-dev qt4-default qt4-dev-tools qt4-qmake libaudio-dev libgtk-3-dev libxt-dev doxygen tcsh
@@ -18,36 +20,32 @@ sudo apt-get install -y rcssserver rcssmonitor rcsslogplayer
 
 cd $home
 echo "Installing librcsc"
-#wget http://c3sl.dl.osdn.jp/rctools/51941/librcsc-4.1.0.tar.gz
-unzip rctools-master.zip
-cd rctools-master/librcsc-4.1.0/
+cd librcsc-4.1.0
 sudo ./configure
 sudo make
 sudo make install
 
 cd $home
 echo "Installing fedit2"
-#git clone https://github.com/KN2C/rctools.git
-cd rctools-master/fedit2-0.0.0/
+cd ../fedit2
 export QT_SELECT=4
 qmake fedit2.pro
 make
 
 cd $home
+cd ../base_teams
+home=$PWD #Store base_teams as current directory
 echo "Installing agent2d"
-#wget http://c3sl.dl.osdn.jp/rctools/55186/agent2d-3.1.1.tar.gz
-cd rctools-master/agent2d-3.1.1/
+cd agent2d-3.1.1/
 sudo ./configure
 sudo make
 
 cd $home
 echo "Installing UvA Trilearn"
-#wget http://inf.ufes.br/~pet/projetos/Simulação_2D/Sim2D/trilearn_base_sources-3.3-v13-by_PET.tar.gz
-tar -xf trilearn_base_sources-3.3-v13-by_PET.tar.gz
 cd trilearn_base_sources-3.3-v13-by_PET
 sudo ./configure
 sudo make
 
 echo "Installation complete"
 echo "Open a new terminal instance or reload terminal (source ~/.bashrc) to apply changes"
-echo "Run fix_decimal_separator(comma).sh IF your system use comma (,) instead of dot (.) as decimal separator"
+echo "Run fix_decimal_separator.sh IF your system use comma (,) instead of dot (.) as decimal separator"
